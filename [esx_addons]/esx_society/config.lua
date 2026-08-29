385f105fb5284d869c324a1071319e8232397671
ASTROWwwW <loulouyt113@gmail.com>
2026-08-29 20:11:37 +0200

    feat(esx_society): grade uniform management from the boss menu

--- [esx_addons]/esx_society/config.lua
+++ [esx_addons]/esx_society/config.lua
@@ -10,3 +10,11 @@ Config.BossGrades = { -- Uncomment and/or add additional grades you want to have
     --['staff2'] = false,
     --['staff3'] = false,
 }
+
+Config.UniformSaveCooldown = 30000 -- Minimum delay (ms) between two uniform saves for the same job.
+Config.UniformComponents = {
+    'tshirt_1', 'tshirt_2', 'torso_1', 'torso_2', 'decals_1', 'decals_2', 'arms', 'arms_2',
+    'pants_1', 'pants_2', 'shoes_1', 'shoes_2', 'mask_1', 'mask_2', 'bproof_1', 'bproof_2',
+    'chain_1', 'chain_2', 'helmet_1', 'helmet_2', 'glasses_1', 'glasses_2', 'watches_1', 'watches_2',
+    'bracelets_1', 'bracelets_2', 'bags_1', 'bags_2', 'ears_1', 'ears_2'
+}
